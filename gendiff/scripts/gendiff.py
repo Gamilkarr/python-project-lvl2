3f27151bb1d07afa0733e72f47135f07773d9d06
Tata <gamilkarr@gmail.com>
2022-04-02 19:37:18 +0300

    some

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,10 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
