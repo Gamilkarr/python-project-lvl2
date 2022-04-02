308edf0f269f15aefaabbbefd82154390c27e93f
Tatyana <102869523+Gamilkarr@users.noreply.github.com>
2022-04-02 21:32:25 +0300

    step_3
    * add generate diff function
    * add test fixtures
    * add asciinema

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Gamilkarr/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Gamilkarr/python-project-lvl2/actions)
+
+[![asciicast](https://asciinema.org/a/IINwPPOolnqGmPSoAy3Esri8b.svg)](https://asciinema.org/a/IINwPPOolnqGmPSoAy3Esri8b)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff import generate_diff
+
 
 
 def main():
@@ -8,6 +10,8 @@ def main():
     parser.add_argument("second_file")
     parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
+
 
 
 if __name__ == "__main__":
